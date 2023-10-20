1f068ba41de6cd8a881d897a2463a05b0b60d083
Darshan <darshan.kb@outlook.com>
2023-10-20 22:25:54 +0530

    spring project created

---
+++ src/main/java/com/game/slotmachine/SlotmachineApplication.java
@@ -0,0 +1,13 @@
+package com.game.slotmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotmachineApplication.class, args);
+	}
+
+}
