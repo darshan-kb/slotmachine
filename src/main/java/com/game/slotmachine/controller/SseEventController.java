64731f74cf98e37dfd52e43bb53b9d0b8a4c7bf8
Darshan <darshan.kb@outlook.com>
2023-11-28 12:17:16 +0530

    separated SSE services

--- src/main/java/com/game/slotmachine/controller/SseEventController.java
+++ src/main/java/com/game/slotmachine/controller/SseEventController.java
@@ -1,22 +1,34 @@
 package com.game.slotmachine.controller;
 
 
-import com.game.slotmachine.service.sseService.SseService;
+import com.game.slotmachine.service.sseService.SseCountdownService;
+import com.game.slotmachine.service.sseService.SseResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
+@RequestMapping("/sse")
 public class SseEventController {
     @Autowired
-    SseService sseService;
+    SseCountdownService sseCountdownService;
+    @Autowired
+    SseResultService sseResultService;
 
-    @GetMapping(path = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter eventStream(){
+    @GetMapping(path = "/countdown", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter countEventStream(){
         SseEmitter emitter = new SseEmitter();
-        sseService.addEmitter(emitter);
+        sseCountdownService.addEmitter(emitter);
+        return emitter;
+    }
+
+    @GetMapping(path = "/result", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter resultEventStream(){
+        SseEmitter emitter = new SseEmitter();
+        sseResultService.addEmitter(emitter);
         return emitter;
     }
 }
--- src/main/java/com/game/slotmachine/service/sseService/SseResultService.java
+++ src/main/java/com/game/slotmachine/service/sseService/SseResultService.java
@@ -1,4 +1,34 @@
 package com.game.slotmachine.service.sseService;
 
+import com.game.slotmachine.model.dto.ResultDTO;
+import com.game.slotmachine.model.payload.ResultPayload;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Service
 public class SseResultService {
+    private final List<SseEmitter> resultEmitters = new CopyOnWriteArrayList<>();
+
+    public void addEmitter(SseEmitter emitter){
+        resultEmitters.add(emitter);
+        emitter.onCompletion(() -> resultEmitters.remove(emitter));
+        emitter.onTimeout(() -> resultEmitters.remove(emitter));
+    }
+
+    public void sendResult(ResultDTO resultDTO){
+        for(SseEmitter emitter : resultEmitters){
+            try{
+                emitter.send(new ResultPayload(resultDTO.slot1(), resultDTO.slot2()));
+            }
+            catch (IOException e){
+                e.printStackTrace();
+                emitter.complete();
+                resultEmitters.remove(emitter);
+            }
+        }
+    }
 }
