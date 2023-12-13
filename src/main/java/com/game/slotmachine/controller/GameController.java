56ed82b01fa9dc561dbc6c7f7b2d8ae740f510bf
Darshan <darshan.kb@outlook.com>
2023-12-13 18:36:06 +0530

    added lastgame endpoint which will send result of last finised game

---
+++ src/main/java/com/game/slotmachine/controller/GameController.java
@@ -0,0 +1,18 @@
+package com.game.slotmachine.controller;
+
+import com.game.slotmachine.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameController {
+    @Autowired
+    GameService gameService;
+    @GetMapping("/game/lastgame")
+    public ResponseEntity<?> getLastFinisedGame(){
+        return new ResponseEntity(gameService.getLastFinisedGame(), HttpStatus.OK);
+    }
+}
